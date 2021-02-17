0426e446d33a186608ba31c5618f4f55d8d3badb
Christoffer Wallenberg <christoffer.wallenberg@zocom.se>
2021-02-17 14:58:56 +0100

    Added example

--- vue-props-example/index.js
+++ vue-props-example/index.js
@@ -1,11 +1,37 @@
+/**
+ * 1. När jag klickar på en todo-item vill jag att ett event skickas till todos(parent)
+ * 2. I todos ska då counter adderas med 1.
+ */
+
+
 //Parent
 Vue.component('todos', {
-    template: `<ul>
-                <todo-item task="Köp kaffe" v-bind:done="false"></todo-item>
-                <todo-item task="Köp kaka" v-bind:done="false"></todo-item>
-                <todo-item task="Brygg kaffe" v-bind:done="false"></todo-item>
-                <todo-item task="Drick kaffe" v-bind:done="false"></todo-item>
-               </ul>`
+    template: `<section>
+                <counter v-bind:count="counter"></counter>
+                <ul>
+                    <todo-item task="Köp kaffe" v-bind:done="false" v-on:done="handleUpdate"></todo-item>
+                    <todo-item task="Köp kaka" v-bind:done="false" v-on:done="handleUpdate"></todo-item>
+                    <todo-item task="Brygg kaffe" v-bind:done="false" v-on:done="handleUpdate"></todo-item>
+                    <todo-item task="Drick kaffe" v-bind:done="false" v-on:done="handleUpdate"></todo-item>
+                </ul>
+               </section>`,
+    data: function() {
+        return {
+            counter: 0
+        }
+    },
+    methods: {
+        handleUpdate: function() {
+            this.counter = this.counter + 1;
+        }
+    }
+});
+
+Vue.component('counter', {
+    props: {
+        count: Number
+    },
+    template: `<h3>Du har klarat av {{ count }} saker på din todo-lista</h3>`
 });
 
 //Child
@@ -14,10 +40,16 @@ Vue.component('todo-item', {
         task: String,
         done: Boolean
     },
-    template: `<li>{{ task }}</li>`,
+    template: `<li v-on:click="update">{{ task }}</li>`,
     data: function() {
         return {
             value: ''
+        }
+    },
+    methods: {
+        update: function() {
+            console.log('Task: ', this.task);
+            this.$emit('done', this.task);
         }
     }
 });
